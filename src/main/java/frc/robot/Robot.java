0df8ac6029e18e9f1d0a94c6028c7de5a78fd55d
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2024-02-10 15:30:36 -0500

    Added test code for swerve module steer motor feedforward
    Added swerve module steer motor feedforward test code that attempts to set the steer motor to the given angular velocity and checks its accuracy.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -150,6 +150,10 @@ public class Robot extends TimedRobot {
 
       SmartDashboard.putNumber("Current Angle", Swerve.getInstance().getSwerveModule(swerveModuleID).getSteerEncoderAngle() * (180.0 / Math.PI));
     }
+
+    Swerve.getInstance().getSwerveModule(0).setSteerMotorVelocity(2 * Math.PI);
+
+    System.out.println(Swerve.getInstance().getSwerveModule(0).getSteerEncoderAngularVelocity());
   }
 
   /** This function is called once when the robot is disabled. */
